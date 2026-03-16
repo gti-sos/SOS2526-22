5af168620812b60ebaa8f1fc1d706c94887e7aba
Celia-Leal <celleasal@alum.us.es>
2026-03-16 09:06:48 +0100

    cambio celia ultimo

--- src/back/indexCLS.js
+++ src/back/indexCLS.js
@@ -1,9 +1,6 @@
-
-
-
-import dataStore from 'nedb';
+import DataStore from 'nedb';
 const BASE_URL_API = "/api/v1";
- let db = new dataStore({ filename: './data/dataCLS.db', autoload: true });
+ let db = new DataStore({ filename: './data/dataCLS.db', autoload: true });
 
 export function loadBackEnd(app) {
 
