90f37607e609093860bdf9f91416f9c19e4bcc10
elemarmor <elemarmor3@alum.us.es>
2026-04-16 16:45:59 +0200

    prueba

--- src/front/src/routes/analytics/analytics.svelte
+++ src/front/src/routes/analytics/analytics.svelte
@@ -1,7 +1,7 @@
 // Ejemplo de lo que debe hacer el script de la grupal
 async function loadAllData() {
     const resAgri = await fetch('/api/v2/global-agriculture-climate-impacts');
-    const resOzono = await fetch('/api/v2/ozone-depleting-substances'); // Ruta de tu compañero
+    const resOzono = await fetch('/api/v2/ozone-depleting-substance-consumptions'); // Ruta de tu compañero
     const resCO2 = await fetch('/api/v2/co2-emissions-stats'); // Ruta de tu otro compañero
 
     if (resAgri.ok && resOzono.ok && resCO2.ok) {
